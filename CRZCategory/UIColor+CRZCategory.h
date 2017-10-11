ab8137567afa104ff626eba1807a6dc805485dec
chenqing512 <chenqing512@sina.com>
2017-10-11 17:28:25 +0800

    添加UIColor

---
+++ CRZCategory/UIColor+CRZCategory.h
@@ -0,0 +1,15 @@
+//
+//  UIColor+CRZCategory.h
+//  TestDemo
+//
+//  Created by ChenQing on 17/10/11.
+//  Copyright © 2017年 ChenQing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (CRZCategory)
+
++ (UIColor *) colorWithHexString: (NSString *) stringToConvert;
+
+@end
